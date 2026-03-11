30073aa20220e3be2ef54a0ad2dc1d3e3acc68fa
Keren Rojas López <233408@ids.upchiapas.edu.mx>
2026-03-11 08:27:29 -0600

    Cambios en la

--- app/src/main/java/com/pulse/plannex/features/location/domain/usecases/GetLocationUpdatesUseCase.kt
+++ app/src/main/java/com/pulse/plannex/features/location/domain/usecases/GetLocationUpdatesUseCase.kt
@@ -1,13 +1,13 @@
 package com.pulse.plannex.features.location.domain.usecases
 
-import com.pulse.plannex.features.location.domain.entities.LocationStatus
+import com.pulse.plannex.features.location.domain.entities.LocationObject
 import com.pulse.plannex.features.location.domain.repositories.LocationRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetLocationUpdatesUseCase(private val repository: LocationRepository) {
-    operator fun invoke(): Flow<LocationStatus> = repository.getLocationUpdates()
+    operator fun invoke(): Flow<LocationObject> = repository.getLocationUpdates()
     
     fun calculateDistance(startLat: Double, startLng: Double, endLat: Double, endLng: Double): Float {
-        return repository.calculateDistance(startLat, startLng, endLat, endLng)
+        return repository.getDistance(startLat, startLng, endLat, endLng)
     }
 }
